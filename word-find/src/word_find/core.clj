df4ea2d3c5f58f1bdf1b01a7c4a8c43577406d25
swashy <roland.macdavid@gmail.com>
2017-01-21 20:23:26 -0500

    simple word-finder which analyzes a grid of random letters to find words

---
+++ word-find/src/word_find/core.clj
@@ -0,0 +1,21 @@
+(ns word-find.core
+  (:gen-class))
+
+
+(def lst '(A O T D L R O W	 
+L C B M U M L U	 
+D R U J D B L J	 
+P A Z H Z Z E F	 
+B C Z E L F H W	 
+R K U L V P P G	 
+A L B L P O P Q	 
+B E M O P P J Y))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+
+  (nth lst 1)
+)
+
+
